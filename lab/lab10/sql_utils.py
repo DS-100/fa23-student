274727ed3f0ac1bf3d1a6b8e6ca329a4ff64ca34
Harshit Jain <harshitjain153@berkeley.edu>
2023-11-03 11:00:30 -0700

    lab10

---
+++ lab/lab10/sql_utils.py
@@ -0,0 +1,35 @@
+import sql
+
+import dill as pickle
+
+from pathlib import Path
+from subprocess import run, PIPE
+
+RESULTS_DIR = "results"
+N_TUPLES = 10
+
+## CODE TAKEN FROM DATA101 PROJECT 3, FA23
+
+def save_results(pkl_fname, *args):
+    pkl_fname = f"{RESULTS_DIR}/{pkl_fname}.pkl"
+    with open(pkl_fname, 'wb') as f:
+        for arg in args:
+            if type(arg) == sql.run.ResultSet:
+                arg = arg.DataFrame() # convert jupysql to dataframe
+            pickle.dump(arg, f)
+    with open(pkl_fname, 'rb') as f:
+        ret_vals = [pickle.load(f) for _ in args]
+    return ret_vals
+    
+# https://stackoverflow.com/questions/18675863/load-data-from-python-pickle-file-in-a-loop
+def load_results(pkl_fname):
+    def pickleLoader(pklFile):
+        try:
+            while True:
+                yield pickle.load(pklFile)
+        except EOFError:
+            pass
+        
+    pkl_fname = f"{RESULTS_DIR}/{pkl_fname}.pkl"
+    with open(pkl_fname, 'rb') as f:
+        return [event for event in pickleLoader(f)]
